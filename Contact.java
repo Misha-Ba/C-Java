331c01a9f4f475036eb0553ddb464d33d2e8f95a
Kamisha <135481543+Misha-Ba@users.noreply.github.com>
2024-03-12 20:54:07 -0400

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,67 @@
+
+public class Contact implements Comparable<Contact> {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public int compareTo(Contact other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone: " + phoneNumber;
+    }
+    
+    public static void bubbleSort(Contact[] contacts) {
+        boolean swapped;
+        for (int i = 0; i < contacts.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < contacts.length - i - 1; j++) {
+                if (contacts[j].compareTo(contacts[j + 1]) > 0) {
+                    Contact temp = contacts[j];
+                    contacts[j] = contacts[j + 1];
+                    contacts[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // Stop iteration if the array is already sorted
+        }
+    }
+    
+    public static void main(String[] args) {
+        Contact[] contacts = {
+            new Contact("Alice", "123-456-7890"),
+            new Contact("Bob", "987-654-3210"),
+            new Contact("Charlie", "555-555-5555")
+        };
+
+        System.out.println("Before sorting:");
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+
+        bubbleSort(contacts);
+
+        System.out.println("\nAfter sorting:");
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  GPA calculator
+//
+//  Created by Kamisha Baijnauth on 5/23/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    string fst, lst, semester;
+    int num;
+    cout << "Hello! Please enter your first and last name: ";
+    cin >> fst >> lst;
+    //cout << endl;
+    cout << "Which semester am I calulating? ";
+    cin >> semester;
+    cout << "For " << semester << " how many classes did you take? ";
+    cin >> num;
+    string clas[num];
+    cout << "List the classes you took \n";
+    for(int i = 0; i < num; i++){
+        cin >> clas[i];
+    }
+    double earned[num];
+    for(int i = 0; i < num; i++){
+        cout << "Enter credit earned for " << clas[i] << " ";
+        cin >> earned[i];
+    }
+    cout << endl;
+    int maxcredit = 0;
+    int max[num];
+    for(int i = 0; i < num; i++){
+        cout << "Enter the max unit for " << clas[i] << " ";
+        cin >> max[num];
+        maxcredit += max[num];
+    }
+    //cout << endl;
+    int sumEarned = 0;
+    
+    for(int i = 0; i < num; i++){
+        sumEarned+= earned[i];
+    }
+    /*for(int i = 0; i < num; i++){
+        maxcredit+= max[i];
+    }*/
+    double GPA = (double)sumEarned/maxcredit;
+    cout << endl;
+    cout << "Sum of credit earned: " << sumEarned << "\n";
+    cout << "Sum of max unit: " << maxcredit << "\n";
+    cout << "Your current GPA is: " << (double)GPA << ".\n";
+    if( GPA <= 4.0 && GPA >= 2.0){
+                cout << "You're on the right track " << fst << "." << endl;
+            }
+    else cout << "You've got some work to do " << fst << "." << endl;
+    return 0;
+}
